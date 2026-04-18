41907c9bfcecd10ab47491581bfe8199d838d333
wcb2006 <wille.borg.c@gmail.com>
2026-04-18 02:40:51 +0200

    Added Testning Course

---
+++ Migrations/20260418003957_TestningCourse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestningCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "Description", "EducationId", "Grade", "Name", "StartDate" },
+                values: new object[] { 4, "Planering, genomförande och uppföljning av tester. Testdriven utveckling (TDD) och olika testmetoder.", 1, "VG", "Testning", new DateTime(2025, 11, 24, 0, 0, 0, 0, DateTimeKind.Utc) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
