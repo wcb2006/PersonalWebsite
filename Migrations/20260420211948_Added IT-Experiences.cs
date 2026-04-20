1358872717719800fc06d830a12875cacc6fe006
wcb2006 <wille.borg.c@gmail.com>
2026-04-20 23:20:41 +0200

    ADded IT-Experiences

---
+++ Migrations/20260420211948_Added IT-Experiences.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PersonalWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedITExperiences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItExperience",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItExperience", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ItExperience",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "C#" },
+                    { 2, ".NET" },
+                    { 3, "SQL" },
+                    { 4, "Git" },
+                    { 5, "API-utveckling" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItExperience");
+        }
+    }
+}
