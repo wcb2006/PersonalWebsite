25d3cb38103b28a98eab1180fd21bda48e8bc520
wcb2006 <wille.borg.c@gmail.com>
2026-04-24 16:26:29 +0200

    Removed Emojis and added a new pfp

--- Models/ITExperience.cs
+++ Models/ITExperience.cs
@@ -3,5 +3,5 @@ namespace PersonalWebsite.Models;
 public class ITExperience
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
